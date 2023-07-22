74416ea6b29ccadaf26e539b34efe7f33e18ad65
SantiOrbe <santiago@orbesoftware.com.ar>
2023-07-22 15:33:57 -0300

    Last Commit

--- TrabajoFinal/settings/base.py
+++ TrabajoFinal/settings/base.py
@@ -44,8 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.posts',
     'apps.usuarios',
+    'apps.posts',
 ]
 
 MIDDLEWARE = [
--- apps/usuarios/apps.py
+++ apps/usuarios/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class UsuariosConfig(AppConfig):
-    name = 'usuarios'
+    name = 'apps.usuarios'
